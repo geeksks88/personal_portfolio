efa121a3a84e8baafd64113af4cff292dfd2f114
Sujal Singh <davasnsujal@gmail.com>
2025-11-14 23:58:27 +0530

    gradle

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,5 +21,5 @@ plugins {
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
-
+rootProject.name = "my_portfolio_main"
 include(":app")
